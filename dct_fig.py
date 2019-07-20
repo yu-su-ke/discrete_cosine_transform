b6dee6889ca945b443975412b15653624a008111
yusuke <mikaduki.2448@gmail.com>
2019-07-20 18:23:35 +0900

    Update dct_fig.py

--- dct_fig.py
+++ dct_fig.py
@@ -8,22 +8,22 @@ class DCT:
     def __init__(self, N):
         self.N = N  # データ数．
         # 2次元離散コサイン変換の基底ベクトルをあらかじめ作っておく
-        self.phi_1d = np.array([self.phi(i) for i in range(self.N)])
+        self.phi_1dimension = np.array([self.phi(i) for i in range(self.N)])
 
-        self.phi_2d = np.zeros((N, N, N, N))
+        self.phi_2dimension = np.zeros((N, N, N, N))
         for i in range(N):
             for j in range(N):
-                phi_i, phi_j = np.meshgrid(self.phi_1d[i], self.phi_1d[j])
-                self.phi_2d[i, j] = phi_i * phi_j
+                phi_i, phi_j = np.meshgrid(self.phi_1dimension[i], self.phi_1dimension[j])
+                self.phi_2dimension[i, j] = phi_i * phi_j
 
     # 2次元離散コサイン変換
     def dct2(self, data):
-        return np.sum(self.phi_2d.reshape(N * N, N * N) * data.reshape(N * N), axis=1).reshape(N, N)
+        return np.sum(self.phi_2dimension.reshape(N * N, N * N) * data.reshape(N * N), axis=1).reshape(N, N)
 
     # 2次元離散コサイン逆変換
     def idct2(self, c, number):
         c[number:, number:] = 0
-        return np.sum((c.reshape(N, N, 1) * self.phi_2d.reshape(N, N, N * N)).reshape(N * N, N * N), axis=0)\
+        return np.sum((c.reshape(N, N, 1) * self.phi_2dimension.reshape(N, N, N * N)).reshape(N * N, N * N), axis=0)\
             .reshape(N, N)
 
     # 基底関数
@@ -48,7 +48,7 @@ class DCT:
 
 def show_image(img, c, y):
     # 元の画像
-    plt.subplot(1, 3, 1)
+    plt.subplot(1, 2, 1)
     plt.imshow(img, cmap="Greys")
     plt.title("original")
     plt.xticks([])
@@ -58,7 +58,7 @@ def show_image(img, c, y):
     # plt.title("cosine transform")
     # plt.xticks([])
     # 復元した画像
-    plt.subplot(1, 3, 3)
+    plt.subplot(1, 2, 2)
     plt.imshow(y, cmap="Greys")
     plt.title("restored")
     plt.xticks([])
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     # 画像のピクセル数を代入
     N = 50
     # 0に丸め込む範囲の指定
-    number = 15
+    number = 10
     # クラスの読み込み
     dct = DCT(N)
 
